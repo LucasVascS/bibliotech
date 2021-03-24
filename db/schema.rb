ad33d1be6da5e3bfd277e1db0507a87a48a29ac2
LucasVasc <lucasvascow@gmail.com>
2021-03-24 12:10:06 -0300

    table relationship and some adjustments in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_133042) do
+ActiveRecord::Schema.define(version: 2021_03_23_192554) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "author"
     t.text "synopsis"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "categoriename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
