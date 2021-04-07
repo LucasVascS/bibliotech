be46a32ab458e72d03f2668cea3c3387b93cfa91
LucasVasc <lucasvascow@gmail.com>
2021-04-07 14:45:49 -0300

    some adjustments in ajax function

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -8,9 +8,12 @@ function openModal(){
   $('.open-modal').on("click", function() {
     $('#exampleModal').modal('show')
     var url = $(this).attr('data-url');
+    var bookName = $(this).attr('data-name');  
     $.ajax({url: url, success: function(result){
       $("#exampleModal .modal-body").html(result);
+      $("#exampleModal .modal-title").html(bookName);
     }});
+    
 
   });
 }
